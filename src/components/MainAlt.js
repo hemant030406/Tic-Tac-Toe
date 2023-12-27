455d05d08ea31e56f3cb4cf44c302198e18d9776
HEMANT PATHAK <122811374+hemant030406@users.noreply.github.com>
2023-12-27 14:40:47 +0530

    draw condition added

--- src/components/MainAlt.js
+++ src/components/MainAlt.js
@@ -79,71 +79,81 @@ function MainAlt(params) {
     }
 
     setInterval(() => {
-        for (let idx in allArr) {
-            if (check(allArr[idx]) == 'Player 1 Won') {
-                document.querySelector('.winner').innerHTML = 'X Won'
-                arr = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
-                setTimeout(() => {
-                    document.querySelector('.winner').innerHTML = ''
-                    reset()
-                }, 2000)
-                break
-            }
-            else if (check(allArr[idx]) == 'Player 2 Won') {
-                document.querySelector('.winner').innerHTML = `O Won`
-                arr = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
-                setTimeout(() => {
-                    document.querySelector('.winner').innerHTML = ''
-                    reset()
-                }, 2000)
-                break
-            }
+            for (let idx in allArr) {
+                if (check(allArr[idx]) == 'Player 1 Won') {
+                    document.querySelector('.winner').innerHTML = 'X Won'
+                    arr = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th','10th']
+                    setTimeout(() => {
+                        document.querySelector('.winner').innerHTML = ''
+                        arr = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
+                        reset()
+                    }, 2000)
+                    break
+                }
+                else if (check(allArr[idx]) == 'Player 2 Won') {
+                    document.querySelector('.winner').innerHTML = `O Won`
+                    arr = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th','10th']
+                    setTimeout(() => {
+                        document.querySelector('.winner').innerHTML = ''
+                        arr = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
+                        reset()
+                    }, 2000)
+                    break
+                }
+                else if ((check(allArr[idx]) == 'None') && arr.length==9) {
+                    document.querySelector('.winner').innerHTML = `Match Drawn`
+                    setTimeout(() => {
+                        document.querySelector('.winner').innerHTML = ''
+                        reset()
+                    }, 2000)
+                    break
+                }
         }
     }, 1000);
 
 
     return (
         <>
-        <h1 id='mantra'>
-            ॐ भूर्भुव॒ स्सुवः॑
-            तत्स॑ वि॒तुर्वरे᳚ण्यं॒
-            भर्गो॑ दे॒वस्य॑ धीमहि
-            धियो॒ यो नः॑ प्रचो॒दया᳚त् ॥
-        </h1>
-        <div id="note">NOTE :- Please wait 1-2 seconds after the game is finished.</div>
-        <div className="tb">
-            <div className="winner"></div>
-            <table>
-                <tr>
-                    <td id='1st' onClick={(e) => draw(e, '1st')}>
-                    </td>
-                    <td id='2nd' onClick={(e) => draw(e, '2nd')}>
-                    </td>
-                    <td id='3rd' onClick={(e) => draw(e, '3rd')}>
-                    </td>
-                </tr>
-                <tr>
-                    <td id='4th' onClick={(e) => draw(e, '4th')}>
-                    </td>
-                    <td id='5th' onClick={(e) => draw(e, '5th')}>
-                    </td>
-                    <td id='6th' onClick={(e) => draw(e, '6th')}>
-                    </td>
-                </tr>
-                <tr>
-                    <td id="7th" onClick={(e) => draw(e, '7th')}>
-                    </td>
-                    <td id="8th" onClick={(e) => draw(e, '8th')}>
-                    </td>
-                    <td id="9th" onClick={(e) => draw(e, '9th')}>
-                    </td>
-                </tr>
-            </table>
-            <div className="res">
-                <input type="submit" value={'Reset'} onClick={() => { reset() }}></input>
-                <input type="submit" value={'Undo'} onClick={() => { Undo() }}></input>
+            <h1 id='mantra'>
+                ॐ भूर्भुव॒ स्सुवः॑
+                तत्स॑ वि॒तुर्वरे᳚ण्यं॒
+                भर्गो॑ दे॒वस्य॑ धीमहि
+                धियो॒ यो नः॑ प्रचो॒दया᳚त् ॥
+            </h1>
+            <div id="note">NOTE :- Please wait 1-2 seconds after the game is finished.</div>
+            <div className="tb">
+                <div className="winner"></div>
+                <table>
+                    <tr>
+                        <td id='1st' onClick={(e) => draw(e, '1st')}>
+                        </td>
+                        <td id='2nd' onClick={(e) => draw(e, '2nd')}>
+                        </td>
+                        <td id='3rd' onClick={(e) => draw(e, '3rd')}>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td id='4th' onClick={(e) => draw(e, '4th')}>
+                        </td>
+                        <td id='5th' onClick={(e) => draw(e, '5th')}>
+                        </td>
+                        <td id='6th' onClick={(e) => draw(e, '6th')}>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td id="7th" onClick={(e) => draw(e, '7th')}>
+                        </td>
+                        <td id="8th" onClick={(e) => draw(e, '8th')}>
+                        </td>
+                        <td id="9th" onClick={(e) => draw(e, '9th')}>
+                        </td>
+                    </tr>
+                </table>
+                <div className="res">
+                    <input type="submit" value={'Reset'} onClick={() => { reset() }}></input>
+                    <input type="submit" value={'Undo'} onClick={() => { Undo() }}></input>
+                </div>
             </div>
-        </div>
         </>
     )
 }
