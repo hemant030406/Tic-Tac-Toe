b82d699e03d4f366006fbde9827a21bf058a7812
hemant030406 <112201024@smail.iitpkd.ac.in>
2024-03-10 00:36:23 +0530

    modified to a newer approach

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import Footer from './components/Footer';
-import Main from './components/Main';
-import MainAlt from './components/MainAlt';
+import Footer from './components/Footer/Footer';
+import Main from './components/Home/Main';
+import MainAlt from './components/Home/Home';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
 
 function App() {
   return (
-      // {/* <Main></Main> */}c
       <>
       <MainAlt></MainAlt>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
       </>
   );
 }
